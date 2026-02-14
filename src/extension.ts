f16400aa34734cb38beda337fb068fffcab2beda
shunki0001 <arkk0807@gmail.com>
2026-02-15 00:24:24 +0900

    refactor: modecontroller

--- src/extension.ts
+++ src/extension.ts
@@ -7,18 +7,10 @@ let modeController: ModeController | undefined;
 
 export function activate(context: vscode.ExtensionContext) {
 
-  // ===== Status Bar =====
-  const statusBar = vscode.window.createStatusBarItem(
-    vscode.StatusBarAlignment.Right,
-    100
-  );
-  statusBar.show();
-
   const decorations = new DecorationManager();
   const configService = new ConfigService();
 
   modeController = new ModeController(
-    statusBar,
     decorations,
     configService
   );
@@ -40,19 +32,6 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     showMode,
     selectionListener,
-    statusBar
+    modeController
   );
-
-  const configListener =
-    vscode.workspace.onDidChangeConfiguration(e => {
-      if (e.affectsConfiguration('vimModeVisualizer.enableNotification')) {
-        console.log('Notification setting changed');
-      }
-    });
-
-  context.subscriptions.push(configListener);
-}
-
-export function deactivate() {
-  modeController?.dispose();
 }
